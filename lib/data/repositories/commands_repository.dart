88f29d72ee40d8df1452b1c4cc06c2093c72d2c9
Alderaan11 <anais.mackow@etu.umontpellier.fr>
2026-01-02 16:47:00 +0100

    Simulator working

--- lib/data/repositories/commands_repository.dart
+++ lib/data/repositories/commands_repository.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:plant_nanny_api/plant_nanny_api.dart';
 import 'package:plant_nanny/core/api_client_provider.dart';
+import 'package:plant_nanny_api/plant_nanny_api.dart';
 
 final commandsRepositoryProvider = Provider<CommandsRepository>((ref) {
   return CommandsRepository(ref.watch(apiClientProvider).getCommandsApi());
@@ -13,7 +13,7 @@ class CommandsRepository {
   Future<void> forceReading(String deviceId) {
     return _api.handlersV1DevicesCommandsPost(
       deviceId: deviceId,
-      commandIn: CommandIn((b) {
+      commandIn: CommandIn((CommandInBuilder b) {
         b.type = CommandType.forceReading;
       }),
     );
@@ -22,7 +22,7 @@ class CommandsRepository {
   Future<void> pump(String deviceId, int durationMs) {
     return _api.handlersV1DevicesCommandsPost(
       deviceId: deviceId,
-      commandIn: CommandIn((b) {
+      commandIn: CommandIn((CommandInBuilder b) {
         b.type = CommandType.pumpWater;
         b.durationMs = durationMs;
       }),
